f33697742060fe414f38b7d2f0855f967df4f7d5
ruchi <ruchiengg2005@gmail.com>
2024-05-08 12:06:54 +0530

    draft

---
+++ src/test/java/in/ruchi/demo20240507/Demo20240507ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ruchi.demo20240507;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20240507ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
